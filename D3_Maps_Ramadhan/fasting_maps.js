60292f11d284ec7e4d66052f5234a0153ae6b585
damarberlari <damarberlari@gmail.com>
2016-06-19 12:57:25 +0700

    add title

--- D3_Maps_Ramadhan/fasting_maps.js
+++ D3_Maps_Ramadhan/fasting_maps.js
@@ -1,27 +1,27 @@
 var dataset =
 [
-{country: "Iceland", city: "Akureyri", start: "01:32", end: "00:17", duration: 22.75, lat: 65.68, lon: -18.11},
-{country: "Iceland", city: "Reykjavik", start: "02:05", end: "23:49", duration: 21.7333333333333, lat: 64.13, lon: -21.89},
-{country: "Sweden", city: "Stockholm", start: "01:52", end: "21:59", duration: 20.1166666666667, lat: 59.33, lon: 18.06},
-{country: "Russia", city: "Moscow", start: "01:45", end: "21:11", duration: 19.4333333333333, lat: 55.75, lon: 37.62},
-{country: "Netherlands", city: "Amsterdam", start: "03:04", end: "22:00", duration: 18.9333333333333, lat: 52.37, lon: 4.89},
-{country: "France", city: "Paris", start: "03:21", end: "21:52", duration: 18.5166666666667, lat: 48.85, lon: 2.35},
-{country: "Romania", city: "Bucharest", start: "03:06", end: "20:59", duration: 17.8833333333333, lat: 44.43, lon: 26.12},
-{country: "Turkey", city: "Istanbul", start: "03:26", end: "20:35", duration: 17.15, lat: 41.01, lon: 28.97},
-{country: "China", city: "Beijing", start: "02:45", end: "19:41", duration: 16.9333333333333, lat: 39.9, lon: 116.4},
-{country: "Japan", city: "Osaka", start: "02:46", end: "19:07", duration: 16.35, lat: 34.69, lon: 135.5},
+{country: "Islandia", city: "Akureyri", start: "01:32", end: "00:17", duration: 22.75, lat: 65.68, lon: -18.11},
+{country: "Islandia", city: "Reykjavik", start: "02:05", end: "23:49", duration: 21.7333333333333, lat: 64.13, lon: -21.89},
+{country: "Swedia", city: "Stockholm", start: "01:52", end: "21:59", duration: 20.1166666666667, lat: 59.33, lon: 18.06},
+{country: "Rusia", city: "Moskow", start: "01:45", end: "21:11", duration: 19.4333333333333, lat: 55.75, lon: 37.62},
+{country: "Belanda", city: "Amsterdam", start: "03:04", end: "22:00", duration: 18.9333333333333, lat: 52.37, lon: 4.89},
+{country: "Perancis", city: "Paris", start: "03:21", end: "21:52", duration: 18.5166666666667, lat: 48.85, lon: 2.35},
+{country: "Rumania", city: "Bukares", start: "03:06", end: "20:59", duration: 17.8833333333333, lat: 44.43, lon: 26.12},
+{country: "Turki", city: "Istanbul", start: "03:26", end: "20:35", duration: 17.15, lat: 41.01, lon: 28.97},
+{country: "Cina", city: "Beijing", start: "02:45", end: "19:41", duration: 16.9333333333333, lat: 39.9, lon: 116.4},
+{country: "Jepang", city: "Osaka", start: "02:46", end: "19:07", duration: 16.35, lat: 34.69, lon: 135.5},
 {country: "Pakistan", city: "Islamabad", start: "03:15", end: "19:17", duration: 16.0333333333333, lat: 33.72, lon: 73.06},
-{country: "Egypt", city: "Cairo", start: "03:18", end: "18:55", duration: 15.6166666666667, lat: 30.06, lon: 31.25},
-{country: "United Arab Emirates", city: "Abu Dhabi", start: "04:06", end: "19:11", duration: 15.0833333333333, lat: 24.47, lon: 54.37},
+{country: "Mesir", city: "Kairo", start: "03:18", end: "18:55", duration: 15.6166666666667, lat: 30.06, lon: 31.25},
+{country: "Uni Emirat Arab", city: "Abu Dhabi", start: "04:06", end: "19:11", duration: 15.0833333333333, lat: 24.47, lon: 54.37},
 {country: "India", city: "Mumbai", start: "04:38", end: "19:16", duration: 14.6333333333333, lat: 19.01, lon: 72.85},
-{country: "Cambodia", city: "Phnom Penh", start: "04:18", end: "18:24", duration: 14.1, lat: 11.56, lon: 104.92},
+{country: "Kamboja", city: "Phnom Penh", start: "04:18", end: "18:24", duration: 14.1, lat: 11.56, lon: 104.92},
 {country: "Nigeria", city: "Lagos", start: "05:15", end: "19:01", duration: 13.7666666666667, lat: 6.44, lon: 3.42},
 {country: "Malaysia", city: "Kuala Lumpur", start: "05:48", end: "19:21", duration: 13.55, lat: 3.23, lon: 101.7},
 {country: "Kenya", city: "Nairobi", start: "05:16", end: "18:34", duration: 13.3, lat: -1.27, lon: 36.81},
 {country: "Indonesia", city: "Jakarta", start: "04:44", end: "17:45", duration: 13.0166666666667, lat: -6.21, lon: 106.84},
 {country: "Zambia", city: "Lusaka", start: "05:13", end: "17:43", duration: 12.5, lat: -15.41, lon: 28.28},
-{country: "Madagascar", city: "Toamasina", start: "04:53", end: "17:14", duration: 12.35, lat: -18.15, lon: 49.4},
-{country: "Mozambique", city: "Chibuto", start: "05:05", end: "17:05", duration: 12, lat: -24.68, lon: 33.53},
+{country: "Madagaskar", city: "Toamasina", start: "04:53", end: "17:14", duration: 12.35, lat: -18.15, lon: 49.4},
+{country: "Mozambik", city: "Chibuto", start: "05:05", end: "17:05", duration: 12, lat: -24.68, lon: 33.53},
 {country: "Australia", city: "Perth", start: "05:45", end: "17:20", duration: 11.5833333333333, lat: -31.9, lon: 115.86},
 {country: "Australia", city: "Hobart", start: "05:56", end: "16:44", duration: 10.8, lat: -42.88, lon: 147.33}
 ]
@@ -71,21 +71,7 @@ var projection = d3.geo.mercator()
 svg = d3.select('.post-content').append('svg')
     .attr('width', width)
     .attr('height', height)
-    
-svg.append("text") //dummytext for font-sizing
-        .attr("y","-20")
-	.text("Osaka")
-        .style("font-size","13px")
-	.each(function(){
-	      textheight = d3.select(this).node().getBoundingClientRect().height;
-	});
-        
-var barHeight = (0.9*height-3*padding)/dataset.length-padding/8;
-var fontSize = barHeight/textheight*12;
 
-var path = d3.geo.path()
-    .projection(projection);
- 
 var svgm = svg.append('g').attr('id','maps');
 var svgc = svg.append('g').attr('id','dots');
 var svgr = svg.append('g').attr('id','rect');
@@ -93,6 +79,54 @@ var svgb = svg.append('g').attr('id','bars');
 var svga = svg.append('g').attr('id','axis');
 var svgl = svg.append('g').attr('id','line');
 var svgt = svg.append('g').attr('id','text');
+   
+var barHeight = (0.9*height-3*padding)/dataset.length-padding/8;
+
+var title = svgt.append("text") //dummytext for font-sizing
+        .attr("id","title")
+        .attr("x", 0.55*width-padding)
+        .attr("y",0.05*height + 3.6*padding)
+        .attr("text-anchor","end")
+        .attr("font-size",13)
+        
+        title
+        .append("tspan")
+        .attr("x", 0.55*width-padding)
+	.text("LAMA PUASA")
+	.each(function(){
+	      textheight = d3.select(this).node().getBoundingClientRect().height;
+	});
+        
+        title
+        .append("tspan")
+        .attr("x", 0.55*width-padding)
+        .attr("dy","0.85em")
+	.text("LAMA")
+        
+        
+var fontSize = barHeight/textheight*12;
+
+svgt.select("#title").attr("font-size",fontSize*3.5);
+
+var subtitle = svgt.append("text") //dummytext for font-sizing
+        .attr("id","subtitle")
+        .attr("x", -padding)
+        .attr("y",4*(barHeight+padding/8)+padding+barHeight/2)
+        .attr("transform","translate("+0.55*width+" "+(0.05*height+barHeight/4)+")")
+        .attr("text-anchor","end")
+        .attr("font-size",fontSize)
+        .text("Sumber: beta.islamicfinder.org")
+        
+      subtitle
+      .append("tspan")
+        .attr("x", -padding)
+        .attr("dy","1.2em")
+        .text("3 Ramadhan 1437H")
+
+var path = d3.geo.path()
+    .projection(projection);
+ 
+
 
 svgm.selectAll("path")
       .data(topojson.object(topology, topology.objects.countries)
@@ -110,7 +144,7 @@ svgt.append("text")
             .attr("y",0)
             .attr("transform","translate("+(0.95*width)+" "+(0.95*height-2.5*padding)+")")
             .attr("font-size", fontSize)
-            .attr("opacity", 0.7)
+            .attr("opacity", 1)
             .attr("text-anchor","end")
             .attr("alignment-baseline","bottom")
             .text("Durasi Puasa (jam)");
