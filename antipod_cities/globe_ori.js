1c885f7ba2bf2e6f600ea2a25e403f3b51d61f54
damarberlari <damarberlari@gmail.com>
2016-07-16 01:13:01 +0700

    bind data to canvas

--- antipod_cities/globe_ori.js
+++ antipod_cities/globe_ori.js
@@ -10,11 +10,6 @@ var phi = d3.scale.linear()
     
 var projection = projection_template([100,0],width / 2, height / 2);
 var projection2 = projection_template([-80,0],width / 2, height / 2);
-var projection3 = d3.geo.orthographic()
-    .scale(475)
-    .translate([width / 2, height / 2])
-    .clipAngle(90)
-    .precision(.1);
 
 var path_0 = d3.geo.path()
     .projection(projection);
@@ -22,67 +17,29 @@ var path_0 = d3.geo.path()
 var path_1 = d3.geo.path()
     .projection(projection2);
 
-var pathgraticule = d3.geo.path()
-    .projection(projection3);
-
-var graticule = d3.geo.graticule();
-
 var svg = d3.select('.d3-content').append('svg')
 	.attr('width', '100%')
 	.attr('height', '100%') 
 	.attr('viewBox',"0 0 1000 1000");
 
-svg.append("defs").append("path")
-    .datum({type: "Sphere"})
-    .attr("class", "sphere")
-    .attr("d", path_0);
-    
-svg.append("defs").append("path")
-    .datum({type: "Sphere"})
-    .attr("class", "sphere")
-    .attr("d", path_1);
 
-
-
-var radialGradient=svg.append("defs").append("radialGradient").attr("cx","50%").attr("cy","50%").attr("r","60%").attr("fx","75%").attr("fy","10%").attr("id","radialgrad")
-
-radialGradient.append("stop")
-    .attr("offset", "0%")
-    .attr("stop-color", "rgb(255,255,255)")
-    .attr("stop-opacity", 0.4);
-
-radialGradient.append("stop")
-    .attr("offset", "100%")
-    .attr("stop-color", "rgb(0,0,0)")
-    .attr("stop-opacity", 0.6);
-    
-svg.append("circle").attr("cx",width/2).attr("cy",height/2).attr("r",475).attr("fill","steelblue")
+   
+svg.append("circle").attr("cx",width/2).attr("cy",height/2).attr("r",475).attr("fill","black")
 
 d3.json("world-50m.json", function(error, world) {
   if (error) throw error;
-
+  var land = topojson.feature(world, world.objects.land);
+  
   svg.insert("path", ".graticule")
-      .datum(topojson.feature(world, world.objects.land))
+      .datum(land)
       .attr("class", "land_0")
       .attr("d", path_0)
       .attr("transform","scale(-1,1) translate(-1000)");
-    svg.insert("path", ".graticule")
-      .datum(topojson.feature(world, world.objects.land))
+      
+  svg.insert("path", ".graticule")
+      .datum(land)
       .attr("class", "land_1")
       .attr("d", path_1)
-//  svg.insert("path", ".graticule")
-//      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
-//      .attr("class", "boundary")
-//      .attr("d", path[i])
-//      .attr("transform",function(){
-//	if(i==1){return "scale(-1,1) translate(-3000)"}else{return null}
-//	});
-
-//svg.append("path")
-//    .datum(graticule)
-//    .attr("class", "graticule")
-//    .attr("d", pathgraticule);
-svg.append("circle").attr("cx",width/2).attr("cy",height/2).attr("r",475).attr("fill","url(#radialgrad)").attr("stroke","white").attr("stroke-width",5)
 });
 d3.timer(rotate);
 d3.select(self.frameElement).style("height", height + "px");
---
+++ antipod_cities/globe_rotate_v2.js
@@ -0,0 +1,91 @@
+var diameter = 750,
+    radius = diameter >> 1,
+    dotRadius = diameter/150,
+    velocity = .02,
+    then = Date.now()
+    landColor = ['#1B5E20','#4CAF50'];
+    
+var dataCity = [
+    {index:0,long:101.44,lat:0.51,color:'crimson',city:"Pekanbaru",country:"Indonesia"},
+    {index:1,long:-78.56,lat:-0.51,color:'gold',city:"Ecuador",country:"Ecuador"},
+    {index:2,long:176.29,lat:-40.42,color:'crimson',city:"Madrid",country:"Spain"},
+    {index:3,long:-3.7,lat:40.42,color:'gold',city:"Weber",country:"New Zealand"},
+    {index:4,long:114.11,lat:22.39,color:'gold',city:"Hong Kong",country:"China"},
+    {index:5,long:-65.89,lat:-22.39,color:'gold',city:"Yujuj",country:"Ecuador"}
+];
+
+var antipodPair = [
+    {from:0,to:1},
+    {from:2,to:3},
+    {from:4,to:5}
+]
+
+var projection = d3.geo.orthographic()
+    .scale(0.9*radius - 2)
+    .translate([radius, radius])
+    .clipAngle(90)
+    .precision(0);
+
+var canvas = d3.select(".d3-content").selectAll("canvas")
+    .data(d3.range(2))
+    .enter().append("canvas")
+    .attr("width", diameter)
+    .attr("height", diameter)
+    .attr("id",function(d,m){return "canvas_"+m});
+
+var path = d3.geo.path()
+    .projection(projection);
+
+d3.json("world-110m.json", function(error, world) {
+  if (error) throw error;
+
+  var land = topojson.feature(world, world.objects.land),
+      globe = {type: "Sphere"};
+
+  d3.timer(function() {
+    var angle = velocity * (Date.now() - then);
+    canvas.each(function(i) {
+      context = this.getContext("2d");
+      path.context(context);
+      context.clearRect(0, 0, diameter, diameter);
+      if(i==0){
+      context.save();
+      context.translate(this.width,0);
+      context.scale(-1,1);
+      rotate = [angle,0];
+      context.beginPath(), path(globe);
+      context.fillStyle = 'black', context.fill()
+      }
+      else{
+      rotate = [angle+180,0]
+      }
+      projection.rotate(rotate);
+      context.beginPath(), path(land),context.fillStyle = landColor[i], context.fill();
+      if(i==0){
+      //draw lines on 1st layer
+      context.beginPath();
+      antipodPair.forEach(function(d){
+	var fromLoc = projection([dataCity[d.from].long,dataCity[d.from].lat]);
+	var toLoc = projection([dataCity[d.to].long,dataCity[d.to].lat]);
+	context.moveTo(fromLoc[0],fromLoc[1]),context.lineTo(toLoc[0],toLoc[1]);
+      })
+      context.lineWidth = 1;
+      context.strokeStyle = 'white';
+      context.stroke();
+      }else{
+      //draw city point on 2nd layer
+      //sample: pekanbaru ecuador
+      dataCity.filter(function(d){return (rotate[0]-90+d.long)%360>180}).forEach(function(d){
+      var loc = projection([d.long,d.lat]);
+      context.beginPath(),context.arc(loc[0],loc[1],dotRadius, 2 * Math.PI, false),context.fillStyle = 'crimson', context.fill();
+      });
+      //draw outer circle
+      context.beginPath(), path.context(context)(globe);
+      context.lineWidth = dotRadius*2.2;
+      context.strokeStyle = 'white';
+      context.stroke();
+      }
+      if(i==0){context.restore()};
+    });
+  });
+});
