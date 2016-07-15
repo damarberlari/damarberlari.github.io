513349d20cfa804041bfec5861f0d8de29caf0b8
damarberlari <damarberlari@gmail.com>
2016-07-15 21:22:59 +0700

    add sample cities

--- antipod_cities/globe_rotate.js
+++ antipod_cities/globe_rotate.js
@@ -5,7 +5,7 @@ var diameter = 900,
     landColor = ['#1B5E20','#4CAF50'];
 
 var projection = d3.geo.orthographic()
-    .scale(radius - 2)
+    .scale(0.8*radius - 2)
     .translate([radius, radius])
     .clipAngle(90)
     .precision(0);
@@ -14,7 +14,8 @@ var canvas = d3.select(".d3-content").selectAll("canvas")
     .data(d3.range(2))
   .enter().append("canvas")
     .attr("width", diameter)
-    .attr("height", diameter);
+    .attr("height", diameter)
+    .attr("id",function(d,m){return "canvas_"+m});
 
 var path = d3.geo.path()
     .projection(projection);
@@ -41,12 +42,49 @@ d3.json("world-110m.json", function(error, world) {
       if(i==0){context.fillStyle = 'black', context.fill()};
       context.beginPath(), path(land),context.fillStyle = landColor[i], context.fill();
       if(i==0){
-      context.beginPath(), context.moveTo(projection([0,30])[0],projection([0,30])[1]),context.lineTo(projection([180,0])[0],projection([180,0])[1]);
-      context.lineWidth = 2;
-      context.strokeStyle = '#424242';
+      context.beginPath();
+      context.moveTo(projection([101.44,0.51])[0],projection([101.44,0.51])[1]),context.lineTo(projection([-78.56,-0.51])[0],projection([-78.56,-0.51])[1]);
+      context.moveTo(projection([176.29,-40.42])[0],projection([176.29,-40.42])[1]),context.lineTo(projection([-3.7,40.42])[0],projection([-3.7,40.42])[1]);
+      context.moveTo(projection([114.11,22.39])[0],projection([114.11,22.39])[1]),context.lineTo(projection([-65.89,-22.39])[0],projection([-65.89,-22.39])[1]);
+      context.moveTo(projection([174.57,-36.13])[0],projection([174.57,-36.13])[1]),context.lineTo(projection([-5.42,36.13])[0],projection([-5.42,36.13])[1]);
+      context.lineWidth = 1;
+      context.strokeStyle = 'white';
       context.stroke();
-      context.beginPath(),context.arc(projection([0,30])[0],projection([0,30])[1],10, 2 * Math.PI, false),context.fillStyle = 'crimson', context.fill();
-      context.beginPath(),context.arc(projection([180,-150])[0],projection([180,-150])[1],10, 2 * Math.PI, false),context.fillStyle = 'gold', context.fill();}
+      };
+      if(i==1){
+      //sample: pekanbaru ecuador
+      if((rotate[0]-90+101.44)%360>180){
+      context.beginPath(),context.arc(projection([101.44,0.51])[0],projection([101.44,0.51])[1],7, 2 * Math.PI, false),context.fillStyle = 'crimson', context.fill();
+      }
+      if((rotate[0]-90+-78.56)%360>180){
+      context.beginPath(),context.arc(projection([-78.56,-0.51])[0],projection([-78.56,-0.51])[1],7, 2 * Math.PI, false),context.fillStyle = 'gold', context.fill();
+      }
+      //sample: madrid weber
+      if((rotate[0]-90+176.29)%360>180){
+      context.beginPath(),context.arc(projection([176.29,-40.42])[0],projection([176.29,-40.42])[1],7, 2 * Math.PI, false),context.fillStyle = 'crimson', context.fill();
+      }
+      if((rotate[0]-90-3.7)%360>180){
+      context.beginPath(),context.arc(projection([-3.7,40.42])[0],projection([-3.7,40.42])[1],7, 2 * Math.PI, false),context.fillStyle = 'gold', context.fill();
+      }
+      //sample: hongkong yujuj
+      if((rotate[0]-90+114.11)%360>180){
+      context.beginPath(),context.arc(projection([114.11,22.39])[0],projection([114.11,22.39])[1],7, 2 * Math.PI, false),context.fillStyle = 'crimson', context.fill();
+      }
+      if((rotate[0]-90-65.89)%360>180){
+      context.beginPath(),context.arc(projection([-65.89,-22.39])[0],projection([-65.89,-22.39])[1],7, 2 * Math.PI, false),context.fillStyle = 'gold', context.fill();
+      }
+      //sample: mangawhai gibraltar
+      if((rotate[0]-90+174.57)%360>180){
+      context.beginPath(),context.arc(projection([174.57,-36.13])[0],projection([174.57,-36.13])[1],7, 2 * Math.PI, false),context.fillStyle = 'crimson', context.fill();
+      }
+      if((rotate[0]-90-5.42)%360>180){
+      context.beginPath(),context.arc(projection([-5.42,36.13])[0],projection([-5.42,36.13])[1],7, 2 * Math.PI, false),context.fillStyle = 'gold', context.fill();
+      }
+      context.beginPath(), path.context(context)(globe);
+      context.lineWidth = 10;
+      context.strokeStyle = 'white';
+      context.stroke();
+      }
       if(i==0){context.restore()};
     });
   });
