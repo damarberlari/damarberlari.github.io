fe9749018ccb2acf1713398c6de90f20a4321ba0
damarberlari <damarberlari@gmail.com>
2017-01-21 13:49:55 +0700

    update d3-slides

--- d3-slides/main_2.js
+++ d3-slides/main_2.js
@@ -1,4 +1,5 @@
-var maxSlides = drawSlides();
+var drawSlides = slideList();
+var maxSlides = drawSlides.length-1;
 var sliding=slide(maxSlides);
 var drawProgress=progressBar(maxSlides);
 
@@ -18,14 +19,23 @@ function progressBar(max){
     }
 }
 
-drawSlides(0);
+drawSlides[0]();
+
+function resetSVG(){
+                d3.selectAll(".circle").transition().attr("r",0);
+                d3.selectAll(".circles").transition().attr("r",0);
+                d3.select(".background").transition().duration(1000).attr("fill","#2196F3");
+                d3.select(".line").transition().duration(0).attr("stroke-dashoffset", "5000");
+                d3.selectAll(".bar").transition().attr("y",720).attr("height",0);
+        }
 
 d3.select("body")
     .on("keydown", function(){
         if(d3.event.keyCode=='39'||d3.event.keyCode=='37'){
         if(d3.event.keyCode=='39'){cursorPointer=sliding(1)}else if(d3.event.keyCode=='37'){cursorPointer=sliding(-1)};
         //console.log(cursorPointer);
-        drawSlides(cursorPointer);
+        resetSVG();
+        drawSlides[cursorPointer]();
         drawProgress(cursorPointer);
         }
     })
@@ -33,37 +43,30 @@ d3.select("body")
     d3.select(".nav-next")
     .on("click",function(){
         cursorPointer=sliding(1);
-        drawSlides(cursorPointer);
+        drawSlides[cursorPointer]();
         drawProgress(cursorPointer);
     });
     
     d3.select(".nav-prev")
     .on("click",function(){
         cursorPointer=sliding(-1);
-        drawSlides(cursorPointer);
+        drawSlides[cursorPointer]();
         drawProgress(cursorPointer);
     });
 
-function drawSlides(drawCounter) {
-    var feature = {
-        transition: d3.transition()
-                    .duration(500),
-        drawSVG: function(){
+function slideList() {
+        var d3transition = d3.transition()
+                        .duration(500);
+                        
+        function drawSVG(){
             d3.select("#d3-content").append('svg')
             .attr("class","svg-master")
-            .attr("id","svg-master")
             .attr('width', '100%')
             .attr('height', '100%') 
             .attr('viewBox',"0 0 1280 720");
             
             d3.select("svg").append("rect").attr("class","background").attr("x",0).attr("y",0).attr("width",1280).attr("height","720");
             d3.select("svg").append("circle").attr("class","circle").attr("cx",10).attr("cy",360).attr("r",0).attr("fill","#FF1744");
-            
-            d3.xml("image/logoD3.svg").get(function(error, xml) {
-                        if (error) throw error;
-                        document.getElementById("svg-master").appendChild(xml.documentElement);
-                        d3.select("#d3-logo").attr("fill-opacity",0).attr("transform","translate(-128,121) translate(640,0) scale(1.4)");
-                });
             
             
             var defs = d3.select("svg")
@@ -123,8 +126,14 @@ function drawSlides(drawCounter) {
             .attr("stroke","#A7FFEB")
             .attr("stroke-width",3);
             
-            
-            
+            d3.select("svg").selectAll(".circles")
+            .data([3,9.5,16])
+            .enter()
+            .append("circle").attr("class","circles")
+            .attr("cx",function(d){return x(d)})
+            .attr("cy",360)
+            .attr("r",0)
+            .attr("fill","#FF1744");
             
             
             d3.select("svg").selectAll(".bar")
@@ -144,18 +153,6 @@ function drawSlides(drawCounter) {
             path
             .attr("stroke-dasharray", "5000 5000")
             .attr("stroke-dashoffset", "5000");
-              
-            d3.select("#svg-master")
-            .selectAll(".circles")
-            .data(d3.range(240))
-            .enter()
-            .append("circle")
-            .attr("class","circles")
-            .attr("cx",function(d,m){return (m % 20)*64+32})
-            .attr("cy",function(d,m){return Math.floor(m/20)*60+30})
-            .attr("r",0)
-            .attr("fill","#FF1744");
-            
             
             d3.select(".svg-master").append("text").attr("class","title-main").attr("x",1220).attr("y",613)
             .attr("filter","url(#title-main-bg)")
@@ -170,104 +167,89 @@ function drawSlides(drawCounter) {
             
             d3.select("svg").append("rect").attr("class","nav nav-next").attr("x",640).attr("y",0).attr("width",640).attr("height",720);
             d3.select("svg").append("rect").attr("class","nav nav-prev").attr("x",0).attr("y",0).attr("width",640).attr("height",720);
-            
-            
-            
-            this.resetSVG();
-        },
-        resetSVG: function(){
-                d3.selectAll(".circle").transition(feature.transition).attr("r",0);
-                d3.selectAll(".circles").transition(feature.transition).attr("r",0);
-                d3.select(".background").transition().duration(1000).attr("fill","#2196F3");
-                d3.select(".line").transition().duration(0).attr("stroke-dashoffset", "5000");
-                d3.selectAll(".bar").transition(feature.transition).attr("y",720).attr("height",0);
-                d3.select("#d3-logo").transition(feature.transition).attr("fill-opacity",0);
-        },
-        drawTitle: function(){
-            
+        }
+        
+        function drawTitle(){
+            console.log("title");
             d3.select(".title-main")
             .text("DATA VISUALIZATION WITH SVG AND D3.JS");
             
             d3.select(".title-side")
-            .text("(Use keyboard arrow or tap the screen to navigate)");
+            .text("");
             
-            d3.select(".circle").transition(feature.transition).attr("cx",10).attr("r",0);
-            },
-        drawSlide1: function(){
+            d3.select(".circle").transition(d3transition).attr("cx",10).attr("r",0);
+            }
             
+        function drawSlide1(){
+            console.log("slide1");
             d3.select(".title-main")
-            .text(" DATA DRIVEN DOCUMENTS (D3)");
+            .text(" DATA VISUALIZATION ");
             
             d3.select(".title-side")
-            .text("A JavaScript library for manipulating documents based on data");
+            .text(" OVERVIEW ");
             
-            d3.select(".background").transition().duration(1000).attr("fill","#212121");
-            d3.select("#d3-logo").transition(feature.transition).attr("fill-opacity",1);
+            d3.select(".circle").transition(d3transition).attr("cx",300).attr("r",150);
             
             //console.log(d3.select(".title-side").node().getBBox());
-            },
-        drawSlide2: function(){
+            }
+            
+        function drawSlide2(){
             d3.select(".title-main")
-            .text("BRING DATA TO LIFE");
+            .text("DATA VISUALIZATION");
             
             d3.select(".title-side")
-            .text("");  
+            .text("..IS EVERYWHERE");  
             
             d3.select(".background").transition().duration(750).attr("fill","#009688");
             d3.select(".line").transition().duration(5000).ease(d3.easeLinear).attr("stroke-dashoffset", 0);
-            },
-        drawSlide3: function(){
+            }
+            
+        function drawSlide3(){
             var y = d3.scaleLinear()
             .domain([0, 1])
             .range([0, 650]);
             
             d3.select(".title-main")
-            .text("BRING DATA TO LIFE");
+            .text("MAKING BAR GRAPH");
             
             d3.select(".title-side")
-            .text("");
-            
+            .text("AS EASY AS");
             d3.select(".background").transition().duration(750).attr("fill","#212121");
-            d3.selectAll(".bar").data(d3.range(20).map(function(){return Math.random()})).attr("y",720).transition(feature.transition).attr("y", function(d){return 720-y(d)}).attr("height",function(d){return y(d)}); 
-            },
-        drawSlide4: function(){
+            d3.selectAll(".bar").data(d3.range(20).map(function(){return Math.random()})).attr("y",720).transition(d3transition).attr("y", function(d){return 720-y(d)}).attr("height",function(d){return y(d)}); 
+            }
+            
+        function drawSlide4(){
             d3.select(".title-main")
-            .text("BRING DATA TO LIFE");
+            .text("DRAWING CIRCLES");
             
             d3.select(".title-side")
-            .text(""); 
-            
+            .text("IN ONE GO");
             d3.select(".background").transition().duration(750).attr("fill","#FFC107");
-            d3.selectAll(".circles").transition(feature.transition).attr("r",20); 
-            },
-        drawSlideEnd: function(){
+            d3.selectAll(".circles").data(d3.range(20)).transition(d3transition).ease(d3.easeBackOut).delay(function(d,m){return m*300;}).attr("r",120); 
+            }
+            
+        function drawSlideEnd(){
             d3.select(".title-main")
             .text("THE END");
             
             d3.select(".title-side")
             .text("");
             d3.select(".background").transition().duration(750).attr("fill","#FFC107").transition().duration(1000).attr("fill","#212121");
-            d3.select(".circle").transition(feature.transition).attr("cx",640).attr("r",250).transition().duration(1000).attr("r",0); 
+            d3.select(".circle").transition(d3transition).attr("cx",640).attr("r",250).transition().duration(1000).attr("r",0); 
             }
-    };
+
+        drawSVG();
 
     var slides = [
-        feature.drawTitle,
-        feature.drawSlide1,
-        feature.drawSlide2,
-        feature.drawSlide3,
-        feature.drawSlide4,
-        feature.drawSlideEnd
+        drawTitle,
+        drawSlide1,
+        drawSlide2,
+        drawSlide3,
+        drawSlide4,
+        drawSlideEnd
     ];
     
-    if(drawCounter!= null){
-        feature.resetSVG();
-        return slides[drawCounter]();
-    }else{
-        //console.log("initiation");
-        feature.drawSVG();
-        return slides.length-1;
-    }
+    return slides;
 };
 
 
