8841d85654282a12267f19d57a6d69cda36d07f9
damarberlari <damarberlari@gmail.com>
2017-02-13 17:27:13 +0700

    Update main.js

--- foreign_oscar/main.js
+++ foreign_oscar/main.js
@@ -324,7 +324,7 @@ var App = React.createClass({
     this.state.dataset.enter(this.state.dataset.data,this.state.dataset.domain);
     d3.select("body")
     .on("keydown", this.update);
-    d3.selectAll(".button-progress").on("click", this.jump).on("mouseover",function(){d3.select(this).transition().duration(500).attr("opacity",1)}).on("mouseout",function(d){d3.select(this).transition().duration(500).attr("opacity",function(){if(d.id=slideStatus){return 1}else{return 0.2}})})
+    d3.selectAll(".button-progress").on("click", this.jump).on("mouseover",function(){d3.select(this).transition().duration(500).attr("opacity",1)}).on("mouseout",function(d){d3.select(this).transition().duration(500).attr("opacity",function(d){if(d.id=slideStatus){return 1}else{return 0.2}})})
   },
   update: function() {
     if(d3.event.keyCode=='39'){
